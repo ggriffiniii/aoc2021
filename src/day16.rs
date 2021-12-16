4b4b469e290c8bea0e249cc4427b18795f7f59a3
Glenn Griffin <ggriffiniii@gmail.com>
2021-12-15 22:59:45 -0800

    day16 part2

--- src/day16.rs
+++ src/day16.rs
@@ -10,8 +10,33 @@ fn sum_version_numbers(packet: &Packet) -> usize {
     packet.version as usize
         + match &packet.lit_or_op {
             LitOrOp::Literal(_) => 0,
-            LitOrOp::Operator(Operator(packets)) => packets.iter().map(sum_version_numbers).sum(),
+            LitOrOp::Sum(packets)
+            | LitOrOp::Product(packets)
+            | LitOrOp::Min(packets)
+            | LitOrOp::Max(packets)
+            | LitOrOp::Gt(packets)
+            | LitOrOp::Lt(packets)
+            | LitOrOp::Eq(packets) => packets.iter().map(sum_version_numbers).sum(),
         }
+}
+
+#[aoc(day16, part2)]
+pub fn part2(input: &str) -> usize {
+    let mut msg = Message::new(input);
+    eval_packet(&parse_packet(&mut msg))
+}
+
+fn eval_packet(packet: &Packet) -> usize {
+    match &packet.lit_or_op {
+        LitOrOp::Literal(lit) => *lit as usize,
+        LitOrOp::Sum(packets) => packets.iter().map(eval_packet).sum(),
+        LitOrOp::Product(packets) => packets.iter().map(eval_packet).product(),
+        LitOrOp::Min(packets) => packets.iter().map(eval_packet).min().unwrap(),
+        LitOrOp::Max(packets) => packets.iter().map(eval_packet).max().unwrap(),
+        LitOrOp::Gt(packets) => (eval_packet(&packets[0]) > eval_packet(&packets[1])) as usize,
+        LitOrOp::Lt(packets) => (eval_packet(&packets[0]) < eval_packet(&packets[1])) as usize,
+        LitOrOp::Eq(packets) => (eval_packet(&packets[0]) == eval_packet(&packets[1])) as usize,
+    }
 }
 
 struct Message<'a> {
@@ -49,31 +74,34 @@ struct Packet {
 
 fn parse_packet(msg: &mut Message) -> Packet {
     let version = msg.consume(3) as u8;
-    let typ = msg.consume(3);
-    let lit_or_op = if typ == 4 {
-        LitOrOp::Literal(parse_literal(msg))
-    } else {
-        LitOrOp::Operator(parse_operator(msg))
+    let typ = msg.consume(3) as u8;
+    let lit_or_op = match typ {
+        0 => LitOrOp::Sum(parse_op_packets(msg)),
+        1 => LitOrOp::Product(parse_op_packets(msg)),
+        2 => LitOrOp::Min(parse_op_packets(msg)),
+        3 => LitOrOp::Max(parse_op_packets(msg)),
+        4 => LitOrOp::Literal(parse_literal(msg)),
+        5 => LitOrOp::Gt(parse_op_packets(msg)),
+        6 => LitOrOp::Lt(parse_op_packets(msg)),
+        7 => LitOrOp::Eq(parse_op_packets(msg)),
+        _ => panic!("unknown packet type"),
     };
     Packet { version, lit_or_op }
 }
 
-#[derive(Debug)]
-struct Literal(u64);
-
-fn parse_literal(msg: &mut Message) -> Literal {
+fn parse_literal(msg: &mut Message) -> u64 {
     let mut lit = 0;
     loop {
         let last = msg.consume(1) == 0;
         lit <<= 4;
         lit |= msg.consume(4);
         if last {
-            return Literal(lit);
+            return lit;
         }
     }
 }
 
-fn parse_operator(msg: &mut Message) -> Operator {
+fn parse_op_packets(msg: &mut Message) -> Vec<Packet> {
     let length_type_id = msg.consume(1);
     if length_type_id == 0 {
         let total_bit_length = msg.consume(15) as usize;
@@ -82,18 +110,21 @@ fn parse_operator(msg: &mut Message) -> Operator {
         while msg.cursor() - start_cursor < total_bit_length {
             packets.push(parse_packet(msg));
         }
-        Operator(packets)
+        packets
     } else {
         let num_subpackets = msg.consume(11);
-        Operator((0..num_subpackets).map(|_| parse_packet(msg)).collect())
+        (0..num_subpackets).map(|_| parse_packet(msg)).collect()
     }
 }
 
 #[derive(Debug)]
 enum LitOrOp {
-    Literal(Literal),
-    Operator(Operator),
+    Literal(u64),
+    Sum(Vec<Packet>),
+    Product(Vec<Packet>),
+    Min(Vec<Packet>),
+    Max(Vec<Packet>),
+    Gt(Vec<Packet>),
+    Lt(Vec<Packet>),
+    Eq(Vec<Packet>),
 }
-
-#[derive(Debug)]
-struct Operator(Vec<Packet>);
